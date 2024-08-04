2a8a6f75afa3a1555209709dabbc4925c63e9143
Eduardo Alves <ctw00306@L-NJ0CQ3J6RP.home>
2024-08-05 00:30:06 +0100

    update port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = ['https://main.d2wbatuus0382b.amplifyapp.com/'];
 
@@ -20,6 +21,6 @@ app.get('/users', (req, res) => {
     res.json([{ email: 'iinesvazfernandes@gmail.com', password: 'XPTO2024' }]);
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
