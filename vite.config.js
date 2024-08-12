2137e446ff402e86a8e02f66e52b3ad0cd609743
Eduardo Alves <ctw00306@L-NJ0CQ3J6RP.local>
2024-08-12 18:32:49 +0100

    add aws-sdk

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import process from 'process';
-import global from 'global';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['aws-sdk'],
+    },
+  },
   define: {
     'process.env': process.env,
     global: 'globalThis', // or you can use the global package
